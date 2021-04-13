7d8a427f71ca3d7fb10b49a8514c4e45132a2e35
axiba <jdxnxmly@gmail.com>
2021-04-13 17:19:48 +0800

    ua 替换

--- func.js
+++ func.js
@@ -1,31 +1,31 @@
 /**
  * 是否ios
  */
-export const isIos = ()=> !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
-
+export const isIos = () =>
+  !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
 
 /**
  * 获取ios版本
  */
-export function getIosVerStr(){
-    let userAgent = navigator.userAgent;
-    let reg = /CPU iPhone OS (.*?) like Mac OS/i;
-    let verStr = userAgent.match(reg)[1];
-    return verStr.replace(/_/g,'.');
+export function getIosVerStr() {
+  let userAgent = navigator.userAgent
+  let reg = /CPU iPhone OS (.*?) like Mac OS/i
+  let verStr = userAgent.match(reg)[1]
+  return verStr.replace(/_/g, '.')
 }
 
 /**
  * 比较两个版本号大小
  * @param {*} ver
  */
-export function isVerLessThan(ver,compareVer){
-    let verArr = ver.split('.');
-    let compareVerArr = compareVer.split('.');
-    let minLength = Math.max(compareVerArr.length,verArr.length);
-    for(let i = 0;i<minLength;i++){
-        if(verArr[i]<compareVerArr[i]){
-            return true;
-        }
+export function isVerLessThan(ver, compareVer) {
+  let verArr = ver.split('.')
+  let compareVerArr = compareVer.split('.')
+  let minLength = Math.max(compareVerArr.length, verArr.length)
+  for (let i = 0; i < minLength; i++) {
+    if (verArr[i] < compareVerArr[i]) {
+      return true
     }
-    return false;
+  }
+  return false
 }
